765b42fd652f6dbd05dca091093f34cdadfe0009
jmbothe <jeffrymbothe@gmail.com>
2018-09-15 15:41:25 -0400

    expose initial state from game logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import { initialState } from 'src/game/tic_tac_toe.js';
 import Board from './Board.jsx';
 
 class App extends Component {
   state = {
-    
-  }
+    ...initialState,
+  };
 
   render() {
     return (
-      <Board />
-    )
+      <Board {...this.state} />
+    );
   }
-};
+}
 
 export default App;
