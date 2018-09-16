bf41362b425cd7f9ecf83a4645a35d57015b5214
jmbothe <jeffrymbothe@gmail.com>
2018-09-15 20:18:02 -0400

    disable tile buttons on  state

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -6,18 +6,24 @@ const TileWrapper = styled.button`
   height: 33.333%;
   background: transparent;
   border: 1px solid black;
-  border-top: ${props => props.index > 2 ? '1px solid black' : 'none'};
-  border-bottom: ${props => props.index < 6 ? '1px solid black' : 'none'};
-  border-left: ${props => props.index % 3 === 0 ? 'none' : '1px solid black'};
-  border-right: ${props => (props.index - 2) % 3 === 0 ? 'none' : '1px solid black'};
+  border-top: ${props => (props.index > 2 ? '1px solid black' : 'none')};
+  border-bottom: ${props => (props.index < 6 ? '1px solid black' : 'none')};
+  border-left: ${props => (props.index % 3 === 0 ? 'none' : '1px solid black')};
+  border-right: ${props =>
+    (props.index - 2) % 3 === 0 ? 'none' : '1px solid black'};
   font-family: "Bowlby One SC", cursive;
   font-size: 3rem;
-  color: ${props => props.occupied === 'X' ? 'red' : 'blue'};
+  color: ${props => (props.occupied === 'X' ? 'red' : 'blue')};
 `;
 
-const Tile = props =>
-  <TileWrapper {...props} onClick={() => props.handleHumanMove(props.index)}>
+const Tile = props => (
+  <TileWrapper
+    {...props}
+    onClick={() => props.handleHumanMove(props.index)}
+    disabled={props.loading}
+  >
     {props.occupied}
-  </TileWrapper>;
+  </TileWrapper>
+);
 
 export default Tile;
